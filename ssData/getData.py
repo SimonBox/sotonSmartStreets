4172331a90c140f80f904e320d1a18f6d7f17e0f
simon <simon@simon-work-laptop.(none)>
2014-03-06 20:20:01 +0000

    okay definately finished now

--- ssData/getData.py
+++ ssData/getData.py
@@ -6,13 +6,16 @@ Created on 6 Mar 2014
 #!/usr/bin/env python
 from retrieve import retrieve
 import sys
+import json
 
-r = retrieve(sys.argv[1],sys.argv[2])
+r = retrieve(float(sys.argv[1]),float(sys.argv[2]))
 [temp,tDist,tTime] = r.getLatestValidTemperature()
-print temp, tDist, tTime
+#print temp, tDist, tTime
 [level,sDist,sTime] = r.getLatestValidGulleySiltLevel()
-print level, sDist, sTime
+#print level, sDist, sTime
 [distance] = r.getNearestPothole()
-print distance
+print json.dumps({"temperature(C)":temp, "thermometerDistance(km)":tDist, "thermometerTime":tTime, "siltLevel(%)":level, "siltSensorDistance(km)":sDist, "siltSensorTime":sTime, "nearestPotholeDistance(km)":distance},indent=4, separators=(',', ': '))
+
+
 
 
