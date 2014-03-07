bcbdf9a1c05b5da45eae08ce2f2317edb63c143d
jb29g08 <jb29g08@soton.ac.uk>
2014-03-07 09:39:33 +0000

    Update getData.py
    Converted to function

--- ssData/getData.py
+++ ssData/getData.py
@@ -8,14 +8,16 @@ from retrieve import retrieve
 import sys
 import json
 
-r = retrieve(float(sys.argv[1]),float(sys.argv[2]))
-[temp,tDist,tTime] = r.getLatestValidTemperature()
-#print temp, tDist, tTime
-[level,sDist,sTime] = r.getLatestValidGulleySiltLevel()
-#print level, sDist, sTime
-[distance] = r.getNearestPothole()
-print json.dumps({"temperature(C)":temp, "thermometerDistance(km)":tDist, "thermometerTime":tTime, "siltLevel(%)":level, "siltSensorDistance(km)":sDist, "siltSensorTime":sTime, "nearestPotholeDistance(km)":distance},indent=4, separators=(',', ': '))
-
+# Sorry, slight change!
+def getSensorData(latitude, longitude):
+  #r = retrieve(float(sys.argv[1]),float(sys.argv[2]))
+  r = retrieve(latitude,longitude)
+  [temp,tDist,tTime] = r.getLatestValidTemperature()
+  #print temp, tDist, tTime
+  [level,sDist,sTime] = r.getLatestValidGulleySiltLevel()
+  #print level, sDist, sTime
+  [distance] = r.getNearestPothole()
+  return json.dumps({"temperature(C)":temp, "thermometerDistance(km)":tDist, "thermometerTime":tTime, "siltLevel(%)":level, "siltSensorDistance(km)":sDist, "siltSensorTime":sTime, "nearestPotholeDistance(km)":distance},indent=4, separators=(',', ': '))
 
 
 
